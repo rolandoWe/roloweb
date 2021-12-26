feceaaae5c2ad8c895d12caabe40b9e8c7c59570
Rolando <riverarolando71@gmail.com>
2021-12-25 21:04:49 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super Eva</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <main class="main">
+       <nav class="nav">
+         <div class="logo">Super Eva</div>
+         <ul class="ul">
+             <span class="close">X</span>
+             <li><a href="#">Sección_1</a></li>
+             <li><a href="#">Sección_2</a></li>
+             <li><a href="#">Sección_3</a></li>
+             <li><a href="#">Sección_4</a></li>
+         </ul>
+         <div class="hamburguesa">..</div>
+       </nav>
+       <header class="header">
+         <!-- <div class="h_text">
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis neque laudantium maiores animi ab vel, quam pariatur nihil non et dolore a eos, ducimus fugit quos mollitia, doloribus temporibus repellat! Ex dolorem exercitationem obcaecati.</p>
+         </div> -->
+         <div class="h_lightBox">
+             <figure class="figura">
+
+             </figure>
+         </div>
+       </header>
+
+        <section class="contenedor">
+         <div class="imagen">
+             <figure>
+                 <img src="imagenes/images.png" alt="">
+             </figure>
+             <h3> <a href="#"> Lorem, ipsum dolor.</a></h3>
+         </div>
+         <div class="imagen">
+             <figure>
+                 <img src="imagenes/buho_libro.png" alt="">
+             </figure>
+             <h3> <a href="#"> Lorem, ipsum dolor.</a></h3>
+         </div>
+         <div class="imagen">
+             <figure>
+                 <img src="imagenes/buho_2_libros.png" alt="">
+             </figure>
+             <h3> <a href="#"> Lorem, ipsum dolor.</a></h3>
+         </div>
+         <div class="imagen">
+             <figure>
+                 <img src="imagenes/buho_corona.png" alt="">
+             </figure>
+             <h3> <a href="#"> Lorem, ipsum dolor.</a></h3>
+         </div>
+         <div class="imagen">
+             <figure>
+                 <img src="imagenes/buho_flor.png" alt="">
+             </figure>
+             <h3> <a href="#"> Lorem, ipsum dolor.</a></h3>
+         </div>
+         <div class="imagen">
+             <figure>
+                 <img src="imagenes/images.png" alt="">
+             </figure>
+             <h3> <a href="#"> Lorem, ipsum dolor.</a></h3>
+         </div>
+        </section>
+
+        <footer>
+
+        </footer>
+
+    </main>
+</body>
+</html>
+
