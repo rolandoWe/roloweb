9eef309840159c506ab1ec6637c89360f1dc79e3
Rolando <riverarolando71@gmail.com>
2021-12-26 20:58:00 -0600

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,23 @@
+<header class="header">
+           <section class="section">
+              <figure>
+                  <img src="imagenes/rolando2.png" alt="">
+              </figure>
+              <h1>Rolando Rivera</h1>
+           </section>
+         <div class="h_text">
+             <ul>
+                <li>Rolando Rivera</li>
+                <li>Desarrollador Front-End</li>
+                <li>Trabajo como Freelance</li>
+                <li>Un poco mas de dos años de experiencias a (2022).</li>
+             </ul>
+             <ul class="ul_dos">
+                <li>Construimos tu pagina/sitio web.</li>
+                <li>Trabajamos con las tecnologías HTML, CSS, Javascript, PHP entre otras.</li>
+                <li>Quieres una página web?.</li>
+                <li> <a href="https://www.facebook.com/rolandoRivera85">Contáctame</a> y trabajámos juntos.</li>
+             </ul>
+         </div>
+        
+       </header>
