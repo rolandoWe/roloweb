5338a35d3fc860285ce542b8e6044d527afb37b8
Rolando <riverarolando71@gmail.com>
2021-12-27 22:55:06 -0600

    Add files via upload

--- template/cabecera.php
+++ template/cabecera.php
@@ -14,7 +14,7 @@
 <body>
     <main class="main">
      <nav class="nav">
-         <div class="logo"><a href="index.php">Rolo<span>Web</span> </a> </div>
+         <div class="logo"><a href="index.php">Rolo.<span>Web</span> </a> </div>
          <ul class="ul">
              <span class="close"><i class="far fa-times-circle"></i></span>
              <li><a href="index.php">Inicio</a></li>
--- template/header.php
+++ template/header.php
@@ -4,19 +4,20 @@
                   <img src="imagenes/rolando2.png" alt="">
               </figure>
               <h1>Rolando Rivera</h1>
+              <p>Desarrollador Web.</p>
            </section>
          <div class="h_text">
              <ul>
-                <li>Rolando Rivera</li>
-                <li>Desarrollador Front-End</li>
-                <li>Trabajo como Freelance</li>
-                <li>Un poco mas de dos años de experiencias a (2022).</li>
+                <li>Hola soy Rolando Rivera</li>
+                <li>Desarrollador Front-end</li>
+                <!-- <li>Freelance</li> -->
+                <li>Autodidacta.</li>
              </ul>
              <ul class="ul_dos">
-                <li>Construimos tu pagina/sitio web.</li>
-                <li>Trabajamos con las tecnologías HTML, CSS, Javascript, PHP entre otras.</li>
+                <li>Construimos tu página/sitio web.</li>
+                <li>Conocimientos avanzados de las tecnologías HTML, CSS, Javascript, PHP entre otras..</li>
                 <li>Quieres una página web?.</li>
-                <li> <a href="https://www.facebook.com/rolandoRivera85">Contáctame</a> y trabajámos juntos.</li>
+                <li> <a href="https://www.facebook.com/rolandoRivera85">Contáctame</a>.</li>
              </ul>
          </div>
         
