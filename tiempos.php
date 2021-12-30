68af3b22fea33ea6fd376505b334843550bb47d5
Rolando <riverarolando71@gmail.com>
2021-12-29 22:18:45 -0600

    Add files via upload

--- tiempos.php
+++ tiempos.php
@@ -10,17 +10,17 @@
             <p>Nicaragua</p>
             <table class="tabla">
                 <tr>
-                    <td>11 AM</td>
+                    <td>11 am</td>
                     <td>Salió</td>
                     <td>91</td>
                 </tr>
                 <tr>
-                    <td>3 PM</td>
+                    <td>3 pm</td>
                     <td>Salió</td>
                     <td>79</td>
                 </tr>
                 <tr>
-                    <td>9 PM</td>
+                    <td>9 pm</td>
                     <td>Salió</td>
                     <td>59</td>
                 </tr>
@@ -49,22 +49,22 @@
             <p>Nicaragua</p>
             <table class="tabla">
                 <tr>
-                    <td>11 PM</td>
+                    <td>11 am</td>
                     <td>Salió</td>
                     <td>65</td>
                 </tr>
                 <tr>
-                    <td>3 PM</td>
+                    <td>3 pm</td>
                     <td>Salió</td>
                     <td>53</td>
                 </tr>
                 <tr>
-                    <td>6 PM</td>
+                    <td>6 pm</td>
                     <td>Salió</td>
                     <td>69</td>
                 </tr>
                 <tr>
-                    <td>9  PM</td>
+                    <td>9  pm</td>
                     <td>Salió</td>
                     <td>11</td>
                 </tr>
@@ -94,17 +94,17 @@
             <p>Nicaragua</p>
             <table class="tabla">
                 <tr>
-                    <td>11 PM</td>
+                    <td>11 am</td>
                     <td>Salió</td>
                     <td>98</td>
                 </tr>
                 <tr>
-                    <td>3 PM</td>
+                    <td>3 pm</td>
                     <td>Salió</td>
                     <td>20</td>
                 </tr> 
                 <tr>
-                    <td>9  PM</td>
+                    <td>9  pm</td>
                     <td>Salió</td>
                     <td>09</td>
                 </tr>
@@ -134,17 +134,17 @@
             <p>Nicaragua</p>
             <table class="tabla">
                 <tr>
-                    <td>11 PM</td>
+                    <td>11 am</td>
                     <td>Salió</td>
                     <td>14</td>
                 </tr>
                 <tr>
-                    <td>3 PM</td>
+                    <td>3 pm</td>
                     <td>Salió</td>
                     <td>28</td>
                 </tr>
                 <tr>
-                    <td>9  PM</td>
+                    <td>9  pm</td>
                     <td>Salió</td>
                     <td>75</td>
                 </tr>
@@ -174,17 +174,17 @@
             <p>Nicaragua</p>
             <table class="tabla">
                 <tr>
-                    <td>11 PM</td>
+                    <td>11 am</td>
                     <td>Salió</td>
                     <td>63</td>
                 </tr>
                 <tr>
-                    <td>3 PM</td>
+                    <td>3 pm</td>
                     <td>Salió</td>
                     <td>92</td>
                 </tr>
                 <tr>
-                    <td>9  PM</td>
+                    <td>9  pm</td>
                     <td>Salió</td>
                     <td>68</td>
                 </tr>
@@ -202,6 +202,46 @@
                    <td>Salió</td>
                    <td>75</td>
                    <td class="col_celda"> <span class="bola_B">Bola</span> </td>
+               </tr>
+               
+            </table>
+        </div>
+ 
+    </div>
+    <div class="grupo_datos">
+        <p class="flecha">Miercoles 29 de Diciembre 2021<i class="fas fa-chevron-circle-down"></i></p>
+        <div class="datos">
+            <p>Nicaragua</p>
+            <table class="tabla">
+                <tr>
+                    <td>11 am</td>
+                    <td>Salió</td>
+                    <td>29</td>
+                </tr>
+                <tr>
+                    <td>3 pm</td>
+                    <td>Salió</td>
+                    <td>61</td>
+                </tr>
+                <tr>
+                    <td>9  pm</td>
+                    <td>Salió</td>
+                    <td>53</td>
+                </tr>
+            </table>
+            <p>Costa Rica</p>
+            <table class="tabla">
+               <tr>
+                   <td>1 pm</td>
+                   <td>Salió</td>
+                   <td>88</td>
+                   <td class="col_celda"> <span class="bola_B">Bola</span> </td>
+               </tr>
+               <tr>
+                   <td>7 pm</td>
+                   <td>Salió</td>
+                   <td>77</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
                </tr>
                
             </table>
