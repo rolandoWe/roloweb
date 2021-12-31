74c48041268885fb196de920dcbf124caf3791ba
Rolando <riverarolando71@gmail.com>
2021-12-31 12:22:33 -0600

    Add files via upload

--- tiempos.php
+++ tiempos.php
@@ -288,6 +288,46 @@
         </div>
  
     </div>
+    <div class="grupo_datos">
+        <p class="flecha">Jueves 31 de Diciembre 2021<i class="fas fa-chevron-circle-down"></i></p>
+        <div class="datos">
+            <p>Nicaragua</p>
+            <table class="tabla">
+                <tr>
+                    <td>11 am</td>
+                    <td>Salió</td>
+                    <td>31</td>
+                </tr>
+                <tr>
+                    <td>3 pm</td>
+                    <td>Salió</td>
+                    <td>--</td>
+                </tr>
+                <tr>
+                    <td>9  pm</td>
+                    <td>Salió</td>
+                    <td>--</td>
+                </tr>
+            </table>
+            <p>Costa Rica</p>
+            <table class="tabla">
+               <tr>
+                   <td>1 pm</td>
+                   <td>Salió</td>
+                   <td>--</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
+               </tr>
+               <tr>
+                   <td>7 pm</td>
+                   <td>Salió</td>
+                   <td>--</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
+               </tr>
+               
+            </table>
+        </div>
+ 
+    </div>
 
    <div class="instante">
        <p>Para ver los resultados al instánte, puedes pinchar en los botónes <a href="https://www.jps.go.cr/productos/nuevos-tiempos">Tica</a> o <a href="https://www.facebook.com/LottoDiaria">Nica</a>.
