2b1bf05087f8aeb0957aa708484228dd3370182e
Rolando <riverarolando71@gmail.com>
2022-01-01 19:00:17 -0600

    Add files via upload

---
+++ cabecera.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RoloWeb</title>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./paginas/jugar_num/num.css">
+
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/videos.css">
+    <link rel="stylesheet" href="query.css">
+    <link rel="stylesheet" href="tiempos.css">
+
+</head>
+<body>
+    <main class="main">
+     <nav class="nav">
+         <div class="logo"><a href="index.php">Rolo.<span>Web</span> </a> </div>
+         <ul class="ul">
+             <span class="close"><i class="far fa-times-circle"></i></span>
+             <li><a href="index.php">Inicio</a></li>
+             <li><a href="#videos">Videos</a></li>
+             <li><a href="#">Sección_3</a></li>
+             <li><a href="#">Sección_4</a></li>
+         </ul>
+         <div class="hamburguesa"><i class="fas fa-bars"></i></div>
+       </nav>
---
+++ pie.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+          <p><i class="far fa-copyright"></i> Creado por: <a href="https://www.facebook.com/rolandoRivera85">Rolando Rivera.</a></p>
+        </footer>
+
+    </main>
+    <script src="index.js"></script>
+    <script src="tiempos.js"></script>
+    <script src="./paginas/jugar_num/num.js"></script>
+    <script src="https://kit.fontawesome.com/f7f35171d9.js" crossorigin="anonymous"></script>
+</body>
+</html>
