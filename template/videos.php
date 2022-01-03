7973d9dbb50eafe9d898d7b03bc153ba080d6b32
Rolando <riverarolando71@gmail.com>
2022-01-02 22:33:31 -0600

    Add files via upload

--- template/videos.php
+++ template/videos.php
@@ -9,5 +9,9 @@
    <iframe width="560" height="315" src="https://www.youtube.com/embed/oQCrO6Sc7kE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p>Color de fondo con Javascript.</p>
    </div>
+   <div class="video">
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/jfmBqk9bJtg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p>Animación con CSS y Javascript.</p>
+   </div>
 
 </div>
