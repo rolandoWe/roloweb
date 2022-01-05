3c5013821e3d2c71de2a83ec16c2c16971009e9f
Rolando <riverarolando71@gmail.com>
2022-01-04 23:12:51 -0600

    Add files via upload

--- tiempos.php
+++ tiempos.php
@@ -418,6 +418,7 @@
         </div>
  
     </div>
+
     <div class="grupo_datos">
         <p class="flecha">Lunes 3 de Enero 2022<i class="fas fa-chevron-circle-down"></i></p>
         <div class="datos">
@@ -456,6 +457,51 @@
                    <td>7 pm</td>
                    <td>Salió</td>
                    <td>40</td>
+                   <td class="col_celda"> <span class="bola_B">Bola</span> </td>
+               </tr>
+               
+            </table>
+        </div>
+ 
+    </div>
+    <div class="grupo_datos">
+        <p class="flecha">Martes 4 de Enero 2022<i class="fas fa-chevron-circle-down"></i></p>
+        <div class="datos">
+            <p>Nicaragua</p>
+            <table class="tabla">
+                <tr>
+                    <td>11 am</td>
+                    <td>Salió</td>
+                    <td>11</td>
+                </tr>
+                <tr>
+                    <td>3 pm</td>
+                    <td>Salió</td>
+                    <td>07</td>
+                </tr>
+                <tr>
+                    <td>6 pm</td>
+                    <td>Salió</td>
+                    <td>63</td>
+                </tr>
+                <tr>
+                    <td>9  pm</td>
+                    <td>Salió</td>
+                    <td>05</td>
+                </tr>
+            </table>
+            <p>Costa Rica</p>
+            <table class="tabla">
+               <tr>
+                   <td>1 pm</td>
+                   <td>Salió</td>
+                   <td>44</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
+               </tr>
+               <tr>
+                   <td>7 pm</td>
+                   <td>Salió</td>
+                   <td>46</td>
                    <td class="col_celda"> <span class="bola_B">Bola</span> </td>
                </tr>
                
