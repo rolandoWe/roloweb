9e9cb64c8d4516deaeec62984d51e744e13fcd45
Rolando <riverarolando71@gmail.com>
2022-02-02 16:17:36 -0600

    Add files via upload

---
+++ tiemXmes/tiemFebrero2022.php
@@ -0,0 +1,95 @@
+
+<!-- <p>probando</p> -->
+
+
+<div class="grupo_datos febrero">
+        <p class="flecha">Martes 1 de Febrero 2022<i class="fas fa-chevron-circle-down"></i></p>
+        <div class="datos">
+            <p>Nicaragua</p>
+            <table class="tabla">
+                <tr>
+                    <td>11 am</td>
+                    <td>Salió</td>
+                    <td>12</td>
+                </tr>
+                <tr>
+                    <td>3 pm</td>
+                    <td>Salió</td>
+                    <td>41</td>
+                </tr>
+                 <tr>
+                    <td>6 pm</td>
+                    <td>Salió</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td>9 pm</td>
+                    <td>Salió</td>
+                    <td>38</td>
+                </tr>
+            </table>
+            <p>Costa Rica</p>
+            <table class="tabla">
+               <tr>
+                   <td>1 pm</td>
+                   <td>Salió</td>
+                   <td>49</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
+               </tr>
+               <tr>
+                   <td>7 pm</td>
+                   <td>Salió</td>
+                   <td>03</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
+               </tr>
+               
+            </table>
+        </div>
+ 
+</div>
+
+<div class="grupo_datos febrero">
+        <p class="flecha">Miércoles 2 de Febrero 2022<i class="fas fa-chevron-circle-down"></i></p>
+        <div class="datos">
+            <p>Nicaragua</p>
+            <table class="tabla">
+                <tr>
+                    <td>11 am</td>
+                    <td>Salió</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td>3 pm</td>
+                    <td>Salió</td>
+                    <td>74</td>
+                </tr>
+                 <!-- <tr>
+                    <td>6 pm</td>
+                    <td>Salió</td>
+                    <td>69</td>
+                </tr> -->
+                <tr>
+                    <td>9 pm</td>
+                    <td>Salió</td>
+                    <td>--</td>
+                </tr>
+            </table>
+            <p>Costa Rica</p>
+            <table class="tabla">
+               <tr>
+                   <td>1 pm</td>
+                   <td>Salió</td>
+                   <td>27</td>
+                   <td class="col_celda"> <span class="bola_R">Bola</span> </td>
+               </tr>
+               <tr>
+                   <td>7 pm</td>
+                   <td>Salió</td>
+                   <td>--</td>
+                   <td class="col_celda"> <span class="bola_B">--</span> </td>
+               </tr>
+               
+            </table>
+        </div>
+ 
+</div>
