6ee685b6486d41b7bb7695658db2aa0e1154ae5e
Rolando <riverarolando71@gmail.com>
2022-06-01 13:46:12 -0600

    Add files via upload

--- includ/footer.php
+++ includ/footer.php
@@ -3,10 +3,10 @@
    <p class="text-center"><i class="fa-solid fa-laptop-code"></i> Creado por: Rolando Rivera</p>
    <p class="text-center"><i class="fa-solid fa-copyright"></i> Todos los Derechos Reservados.</p>
    <ul>
-     <h5>Contacto:</h5>
-     <li><i class="fa-solid fa-envelope"></i> riverarolando71@gmail.com</li>
-     <li><i class="fa-brands fa-facebook"></i> <a href="" class="">Rolando Rivera</a></li>
-     <li><i class="fa-brands fa-whatsapp-square"></i> +506 7287-7301</li>
+     <h5 class="fs-2">Contacto:</h5>
+     <li><i class="fa-solid fa-envelope mt-2"></i> riverarolando71@gmail.com</li>
+     <li><i class="fa-brands fa-facebook mt-2"></i> <a href="" class="">Rolando Rivera</a></li>
+     <li><i class="fa-brands fa-whatsapp-square mt-2"></i> +506 7287-7301</li>
    </ul>
  </footer>
 
