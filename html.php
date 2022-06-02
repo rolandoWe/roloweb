2bf0dfcbcfd39100bb14b4fab68734fd72ccd525
Rolando <riverarolando71@gmail.com>
2022-06-01 20:40:17 -0600

    Add files via upload

---
+++ html.php
@@ -0,0 +1,34 @@
+
+<?php include('includ/head.php')?>
+<div class="container-fluid">
+    <div class="container-fluid bg-ligh">
+        <h2 class="pt-4 text-light">Que es HTML?</h2>
+    <p>HTML es el lenguaje con el que se define el contenido de las páginas web. Básicamente se trata de un conjunto de etiquetas que sirven para definir el texto y otros elementos que compondrán una página web, como imágenes, listas, vídeos, etc.</p>
+    <div class="img-html mb-4">
+        <img src="imagenes/maquetacion-email-html.png" alt="">
+    </div>
+    <p>El HTML se creó en un principio con objetivos divulgativos de información con texto y algunas imágenes. No se pensó que llegara a ser utilizado para crear área de ocio y consulta con carácter multimedia (lo que es actualmente la web), de modo que, el HTML se creó sin dar respuesta a todos los posibles usos que se le iba a dar y a todos los colectivos de gente que lo utilizarían en un futuro. Sin embargo, pese a esta deficiente planificación, si que se han ido incorporando modificaciones con el tiempo, estos son los estándares del HTML. Numerosos estándares se han presentado ya. El HTML 4.01 es el último estándar a febrero de 2001. Actualización a mayo de 2005, en estos momentos está apunto de presentarse la versión 5 de HTML, de la que ya se tiene un borrador casi definitivo.</p>
+    <p>El HTML es un lenguaje de marcación de elementos para la creación de documentos hipertexto, muy fácil de aprender, lo que permite que cualquier persona, aunque no haya programado en la vida, pueda enfrentarse a la tarea de crear una web. HTML es fácil y pronto podremos dominar el lenguaje. Más adelante se conseguirán los resultados profesionales gracias a nuestras capacidades para el diseño y nuestra vena artista, así como a la incorporación de otros lenguajes para definir el formato con el que se tienen que presentar las webs, como CSS.</p>
+    <p>Una vez conocemos el concepto de HTML os vamos a adelantar algunas cosas más. Este lenguaje se escribe en un documento de texto, por eso necesitamos un editor de textos para escribir una página web. Así pues, el archivo donde está contenido el código HTML es un archivo de texto, con una peculiaridad, que tiene extensión .html o .htm (es indiferente cuál utilizar). De modo que cuando programemos en HTML lo haremos con un editor de textos, lo más sencillo posible y guardaremos nuestros trabajos con extensión .html, por ejemplo mipagina.html</p>
+   <span>Fuente: <a href="https://desarrolloweb.com/">https://desarrolloweb.com/</a></span>
+
+   <div class="notaHtml">
+       <h3 class="mt-5 mb-2 text-light">Sabias como mostar etiquetas HTML sin que el editor lo interprete?</h3>
+       <h5>Aqui te dejamos unos tips:</h5>
+       <ul>
+           <li>Para mostrar las llaves <strong>&lt;&gt;</strong> menor y mayor que; debemos escribir lo siguiente: </li>
+           <li><strong>&</strong><strong>lt</strong> Equivale a: <strong>&lt;</strong>.</li>
+           <li><strong>&</strong><strong>gt</strong> Equivale a: <strong>&gt;</strong>.</li>
+           <li>Para escribir simbolos como <strong>&#8364;</strong> o <strong>&#8353;</strong>, etc, etc; debemos escribir <strong>#&</strong> mas el codigo numerico que por lo general consta de 4 digitos que varían segun el simbolo requerido Ej:</li>
+           <li><strong>&#</strong><strong>8364;</strong> Equivale a <strong> &#8364;</strong>. (Al final del codigo debemos poner comas dobles <strong>;</strong>)</li>
+           <li><strong>&#</strong><strong>8353;</strong> Equivale a <strong>&#8353;</strong></li>
+           <li><strong>&#</strong><strong>8453;</strong> Equivale a <strong>&#8453;</strong></li>
+           <li><strong>&#</strong><strong>8373;</strong> Equivale a <strong>&#8373;</strong>.</li>
+           <li><strong>&#</strong><strong>8471;</strong> Equivale a <strong>&#8471;</strong>.</li>
+       </ul>
+   </div>
+
+    </div>
+</div>
+
+<?php include('includ/footer.php')?>
---
+++ js.php
@@ -0,0 +1,17 @@
+
+<?php include('includ/head.php')?>
+<div class="container-fluid">
+    <h2 class="text-light">Que es JavaScript?</h2>
+    <div class="img-js">
+        <img src="imagenes/que-es.js.png" alt="">
+    </div>
+    <p>JavaScript es un lenguaje de programación o de secuencias de comandos que te permite implementar funciones complejas en páginas web, cada vez que una página web hace algo más que sentarse allí y mostrar información estática para que la veas, muestra oportunas actualizaciones de contenido, mapas interactivos, animación de Gráficos 2D/3D, desplazamiento de máquinas reproductoras de vídeo, etc., puedes apostar que probablemente JavaScript está involucrado. Es la tercera capa del pastel de las tecnologías web estándar, dos de las cuales (HTML y CSS) hemos cubierto con mucho más detalle en otras partes del Área de aprendizaje.</p>
+    <div class="img_js">
+        <img src="imagenes/js.png" alt="">
+    </div>
+    <p>HTML es el lenguaje de marcado que usamos para estructurar y dar significado a nuestro contenido web, por ejemplo, definiendo párrafos, encabezados y tablas de datos, o insertando imágenes y videos en la página.</p>
+    <p>CSS es un lenguaje de reglas de estilo que usamos para aplicar estilo a nuestro contenido HTML, por ejemplo, establecer colores de fondo y tipos de letra, y distribuir nuestro contenido en múltiples columnas.</p>
+    <p>JavaScript es un lenguaje de secuencias de comandos que te permite crear contenido de actualización dinámica, controlar multimedia, animar imágenes y prácticamente todo lo demás. (Está bien, no todo, pero es sorprendente lo que puedes lograr con unas pocas líneas de código JavaScript).</p>
+    <span>Fuente: <a href="https://developer.mozilla.org/">https://developer.mozilla.org/</a></span>
+</div>
+<?php include('includ/footer.php')?>
